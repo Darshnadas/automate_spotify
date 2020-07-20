e247bcbc6452e0de3e989c19e71bc0a1549d003a
Darshna Das <darshna.das98@gmail.com>
2020-07-20 19:53:54 +0530

    updated create_playlist.py

--- create_playlist.py
+++ create_playlist.py
@@ -1,8 +1,6 @@
 import json
+
 import requests
-
-
-
 
 
 class CreatePlaylist:
@@ -29,7 +27,7 @@ class CreatePlaylist:
         })
 
         query = "https://api.spotify.com/v1/users/{}/playlists".format(self.user_id)
-        response = request.post(
+        response = requests.post(
             query,
             data = request_body,
             headers = {
@@ -45,7 +43,26 @@ class CreatePlaylist:
 
 
 #Step 4: Search For The Song
-    def get_spotify_url(self):
-        pass
+    def get_spotify_uri(self, song_name, artist):
+
+        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+            song_name,
+            artist
+        )
+        response = requests.get(
+            query,
+            data = request_body,
+            headers = {
+                "Content-Type":"application/jason",
+                "Authorization":"Bearer {}".format(spotify_token)
+            }
+        )    
+        response_json = response.json()
+        songs = response_json['tracks']['items']
+
+        uri = songs[0]['uri']
+
+        return uri  
 
 
+        )
