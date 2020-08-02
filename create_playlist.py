69a2a8967d507527e1354e89fe8b2830d72a486a
Darshna Das <darshna.das98@gmail.com>
2020-08-03 01:17:48 +0530

    updated create_playlist.py

--- create_playlist.py
+++ create_playlist.py
@@ -15,7 +15,10 @@ from secrets import spotify_token,spotify_user_id
 class CreatePlaylist:
     
     def __init__(self):
+        self.user_id = spotify_user_id
+        self.spotify_token = spotify_token
         self.youtube_client = self.get_youtube_client()
+        self.all_song_info = {}
         
 
 #Step 1: Log Into YouTube
@@ -44,9 +47,39 @@ class CreatePlaylist:
 
         
 
-#Step 2: Grab our liked videos
+#Step 2: Grab our liked videos & creating a Dictionary of important song information
     def get_liked_videos(self):
-        pass
+        request = self.youtube_client.videos().list(
+            part="snippet,contentDetails,statistics",
+            myRating="like"
+        )
+        response = request.execute()
+
+        #collect each video and get important information
+        for item in response['items']:
+            video_titile = item['snippet']['titile']
+            youtube_url = "https://www.youtube.com/watch?v={}".format(item['id'])
+
+            #use youtube_dl to collect the song name and the artist name
+            video = youtube_dl.YoutubeDL({}).extract_info(youtube_url, download = False)
+            song_name = video["track"]
+            artist = video["artist"]
+
+            #save all important info
+            self.all_song_info[video_titile]={
+                "youtube_url":youtube_url,
+                "song_name":song_name,
+                "artist":artist,
+
+                #add the uri, easy to get song to put into playlist
+                "spotify_uri": self.get_spotify_uri(song_name,artist)
+
+
+            }
+
+
+
+
 
 #Step 3: Create A New Playlist
     def create_playlist(self):
@@ -82,7 +115,7 @@ class CreatePlaylist:
             artist
         )
         response = requests.get(
-            query,
+            query, 
             headers = {
                 "Content-Type":"application/json",
                 "Authorization":"Bearer {}".format(spotify_token)
@@ -94,3 +127,14 @@ class CreatePlaylist:
         uri = songs[0]['uri']
 
         return uri  
+
+#Step 5: Add this song into new Spotify playlist
+    def add_song_to_playlist(self):
+        # populate songs in our dictionary
+
+        # collect all of uri
+         
+        # create a new playlist
+        
+        #  add all songs into new playlist 
+        pass    
